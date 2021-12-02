8f2330d7d9525041f8fab1cfd73435c5f9b8fc23
graymatterj <github@thegrays.co.uk>
2021-12-02 08:01:49 +0000

    Add common method for running solutions against input files

---
+++ common/common.go
@@ -0,0 +1,20 @@
+package common
+
+import (
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+)
+
+func RunFunctionAgaisntFile(functionToExecute func(*bufio.Scanner) string, filePath string) {
+    file, err := os.Open(filePath)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    scanner := bufio.NewScanner(file)
+    fmt.Println(functionToExecute(scanner))
+
+    file.Close()
+}
